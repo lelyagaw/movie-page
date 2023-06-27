9ca917b61a3f94a496bca25d865dc9887e30041c
le yagaw <sawlelyagaw@gmail.com>
2023-06-27 15:30:45 +0700

    card design

--- components/Card.js
+++ components/Card.js
@@ -4,15 +4,15 @@ import React from 'react'
 export default function Card({result}) {
     const BASE_URL = "https://image.tmdb.org/t/p/original/";
   return (
-    <div>
+    <div className='p-3 cursor-pointer hover:text-yellow-200 active:text-red-400'>
         <Image layout='responsive'
           src={BASE_URL+result.backdrop_path}
             height={100}
             width={200}
         />
-        <div className=''>
-            <p className='truncate'>{result.overview}</p>
-            <h2>{result.title || result.name}</h2>
+        <div className='p-2'>
+            <p className='truncate text-lg'>{result.overview}</p>
+            <h2 className='text-lg font-bold'>{result.title || result.name}</h2>
             <p>{result.release_date || result.first_air_date}</p>
         </div>
     </div>
--- components/Results.js
+++ components/Results.js
@@ -3,7 +3,7 @@ import Card from './Card'
 
 export default function Results({results}) {
   return (
-    <div>
+    <div className='bg-gray-700 text-gray-200 select-none sm:grid grid-cols-2 lg:grid-cols-3 xlg:gid-cols-4'>
       {results.map((result) => (
         <Card key={result.id} result={result}/>
       ))}
